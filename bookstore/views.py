00acd362a442c5745d2bdf33304e7b4219254f9f
FarjanaEity48 <marjan.eityt39@gmail.com>
2020-10-03 00:48:31 +0600

    third commit

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def home(request):
-    return render(request, 'bookstore/home.html')
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'bookstore/home.html',context)
