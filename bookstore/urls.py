b60bbb72951863fc43c86476f7c092203465b2fa
FarjanaEity48 <marjan.eityt39@gmail.com>
2020-10-05 22:19:14 +0600

    final

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('book_detail/<int:pk>/',views.book_detail,name ="book_detail"),
+
 ]
--- bookstore/views.py
+++ bookstore/views.py
@@ -6,3 +6,15 @@ def home(request):
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'bookstore/home.html',context)
+
+
+
+def book_detail(request, pk):
+    book= Book.objects.get(pk=pk)
+    context = {
+        "book": book,
+    }
+    return render(request, "bookstore/book_detail.html", context)
+
+
+
